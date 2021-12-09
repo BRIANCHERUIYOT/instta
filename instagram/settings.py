b16c7286302d273fe4f9d7b91ae91564cfc6179d
BRIANCHERUIYOT <briancheruiyot@student.moringaschool.com>
2021-12-09 13:11:38 +0300

    add files default storage

--- instagram/settings.py
+++ instagram/settings.py
@@ -163,8 +163,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 django_heroku.settings(locals())
