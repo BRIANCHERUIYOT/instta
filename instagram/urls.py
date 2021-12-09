89ea4c6d58f1cc70ae5fbd6b45271eca8716542a
BRIANCHERUIYOT <briancheruiyot@student.moringaschool.com>
2021-12-09 10:45:40 +0300

    adds registration templates and migration

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,7 +1,7 @@
 """instagram URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,15 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
 from django.contrib import admin
 from django.urls import path,include
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('insta.urls')),
-    path('about/',include('insta.urls')),
-    path('accounts/', include('registration.backends.simple.urls')),
-    path('logout/', auth_views.LogoutView.as_view()), 
+    path('',include('posts.urls')),
+
 ]
--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/moringa/django/instta/virtual"
+set -gx VIRTUAL_ENV "/home/moringa/Music/profile/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
